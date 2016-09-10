fd8fd60f3a5a62152be087e44625ecc9c7d67416
Viveckh <vpandey@ramapo.edu>
2016-09-09 22:18:16 -0400

    Setting up workspace, targetver, stdafx header files for library imcrm1pjbports

---
+++ Daicey/stdafx.h
@@ -0,0 +1,30 @@
+//
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN		// Excludes rarely-used stuff from Windows headers
+#include <stdio.h>
+#include <tchar.h>
+
+// Additional headers that the program requires are listed here
+// Standard include files.
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <iomanip>
+#include <windows.h>
+#include <map>
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
